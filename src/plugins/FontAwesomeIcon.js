98d2bfd2ab329be2e00557e67eb423feabbc5135
Daniel da Rocha <danrocha@gmail.com>
2019-06-11 07:30:17 +0200

    add star icon

--- src/plugins/FontAwesomeIcon.js
+++ src/plugins/FontAwesomeIcon.js
@@ -1,8 +1,8 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faMinus, faStar } from "@fortawesome/free-solid-svg-icons";
 
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faPlus, faMinus);
+library.add(faPlus, faMinus, faStar);
 
 export default FontAwesomeIcon;

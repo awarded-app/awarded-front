5b3de4b6fb9dcc816bdea991e00e13ba45b3e328
Daniel da Rocha <danrocha@gmail.com>
2019-06-11 07:30:32 +0200

    create decoration components

---
+++ src/components/PlusSign.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-8">
+    <font-awesome-icon icon="plus" class="w-12" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PlusSign"
+};
+</script>
+
+<style lang="scss" scoped></style>

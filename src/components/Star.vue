a7a748143b2b182bbdcf50af2e6ff142cb9a0629
Daniel da Rocha <danrocha@gmail.com>
2019-07-18 21:54:53 +0200

    add data props to functional component

--- src/components/Star.vue
+++ src/components/Star.vue
@@ -1,7 +1,13 @@
 <template functional>
   <font-awesome-icon
     :icon="props.winner ? 'star' : ['far', 'star']"
-    class="text-xs"
-    :class="props.winner ? 'text-yellow-500' : 'text-gray-800'"
+    :class="[
+      data.class,
+      data.staticClass,
+      props.winner ? 'text-yellow-500' : 'text-gray-800'
+    ]"
+    :style="[data.style, data.staticStyle]"
+    v-bind="data.attrs"
+    v-on="listeners"
   />
 </template>

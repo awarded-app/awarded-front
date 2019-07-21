b47b9e44976780cec23c9988f115b3019d01888a
Daniel da Rocha <danrocha@gmail.com>
2019-07-21 11:38:16 +0200

    improve movie-link component

--- src/components/MovieLink.vue
+++ src/components/MovieLink.vue
@@ -3,6 +3,24 @@
     :to="`/movie/${props.movieId}/${props.movieTitle}`"
     tag="a"
     class="title-link"
-    >{{ props.movieTitle }}</router-link
+    :class="[data.class, data.staticClass]"
+    :style="[data.style, data.staticStyle]"
+    v-bind="data.attrs"
+    v-on="listeners"
+    ><slot>{{ props.movieTitle }}</slot></router-link
   >
 </template>
+<script>
+export default {
+  props: {
+    movieId: {
+      type: Number,
+      required: true
+    },
+    movieTitle: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>

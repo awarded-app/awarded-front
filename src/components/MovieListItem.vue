f072dc3cb3ab9bb29521698f5c23fa29886d478c
Daniel da Rocha <danrocha@gmail.com>
2019-07-22 21:16:26 +0200

    fix fragments locations

---
+++ src/components/MovieListItem.vue
@@ -0,0 +1,59 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import gql from "graphql-tag";
+export default {
+  fragments: {
+    movie: gql`
+      fragment movie on Movie {
+        nodeId
+        id
+        title
+        homepage
+        imdbId
+        originalLanguage
+        originalTitle
+        overview
+        releaseDate
+        runtime
+        tagline
+        tmdbId
+        asin {
+          us
+          de
+        }
+        movieCountries {
+          totalCount
+          nodes {
+            country {
+              nodeId
+              id
+              name
+              code
+            }
+          }
+        }
+        posterPath
+        moviePosters {
+          totalCount
+          nodes {
+            id
+            filename
+          }
+        }
+        movieBackdrops {
+          nodes {
+            id
+            filename
+          }
+          totalCount
+        }
+      }
+    `
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>

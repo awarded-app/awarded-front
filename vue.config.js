9cb6cfd941dfd3f20bfb0564a036dfa4cf7c2731
Daniel da Rocha <danrocha@gmail.com>
2019-07-24 09:15:56 +0200

    fix purgecss

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+const glob = require("glob-all");
+const path = require("path");
+
 module.exports = {
   devServer: {
     proxy: {
@@ -6,5 +10,17 @@ module.exports = {
         changeOrigin: true
       }
     }
+  },
+  configureWebpack: {
+    // Merged into the final Webpack config
+    plugins: [
+      new PurgecssPlugin({
+        paths: glob.sync([
+          path.join(__dirname, "./src/index.html"),
+          path.join(__dirname, "./**/*.vue"),
+          path.join(__dirname, "./src/**/*.js")
+        ])
+      })
+    ]
   }
 };

c6156dc10bad009d91875a8c68896a0252f78e2a
Daniel da Rocha <danrocha@gmail.com>
2019-07-27 11:12:44 +0200

    add about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,55 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<template functional>
+  <article class="indented md:w-2/3 lg:w-1/3">
+    <h2 class="mb-8">What is this?</h2>
+    <section>
+      <p>
+        I started <span class="text-white">Awarded</span> as a present for my dear
+        <span class="text-white">mom</span>. Every year we would sit together through movie sessions
+        where we would watch all nominated movies for the <span class="text-white">Oscars</span>, or
+        the winner movies of <span class="text-white">Cannes</span> or
+        <span class="text-white">Berlinale</span> Festivals.
+      </p>
+      <p>
+        She would keep track of all movies in a little
+        <span class="text-white">list</span>, and tick one by one as we watched them. Sometimes we
+        missed one or two, or the list would get lost among the house's papers and she would start
+        it all over again.
+      </p>
+      <p>
+        I then had the idea to do this for her. A place where she could find
+        <span class="text-white">all nominations, for all awards, for all years</span>. Easy, fast,
+        always available. There was also the potential this could be useful for many people. So I
+        got on building.
+      </p>
+      <h3>What's the vision?</h3>
+      <p>
+        What you see here is still a work in progress. The idea is to expand not only adding more
+        awards and movies, but also
+        <span class="text-white">books, games, products, buildings, TV shows</span>; anything that
+        was nominated or got a prize has the potential to come to Awarded.
+      </p>
+      <h3>How to get updates?</h3>
+      <p>
+        To get (very occasional) updates about new categories or when the latest nominations are
+        online,
+        <a
+          href="http://eepurl.com/dnFggL"
+          target="_blank"
+          class="link-external title-link  text-white"
+          >subscribe to my little newsletter</a
+        >.
+      </p>
+      <p>
+        And please, help spread the word!
+      </p>
+    </section>
+  </article>
 </template>
+<style scoped>
+p {
+  @apply mb-4 text-gray-500;
+}
+h3 {
+  @apply mt-8;
+}
+</style>

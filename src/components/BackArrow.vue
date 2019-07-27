c62f73841c280fa05a66621d01c2a78ce953d4fe
Daniel da Rocha <danrocha@gmail.com>
2019-07-27 11:14:14 +0200

    fix backArrow functional component

--- src/components/BackArrow.vue
+++ src/components/BackArrow.vue
@@ -1,5 +1,8 @@
 <template functional>
-  <router-link :to="props.to" tag="a" class="text-base hover:text-yellow-500"
+  <router-link :to="props.to" tag="a" class="text-base hover:text-yellow-500" :class="[data.class, data.staticClass]"
+    :style="[data.style, data.staticStyle]"
+    v-bind="data.attrs"
+    v-on="listeners"
     >&larr;</router-link
   >
 </template>

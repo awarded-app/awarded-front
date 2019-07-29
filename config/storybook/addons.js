98404ebe4c98c25f7c7652c0fcaada4844db4cc7
Daniel da Rocha <danrocha@gmail.com>
2019-07-29 19:07:22 +0200

    storybook cleanup

--- config/storybook/addons.js
+++ config/storybook/addons.js
@@ -1,5 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import '@storybook/addon-actions/register'
-import '@storybook/addon-knobs/register'
-import '@storybook/addon-links/register'
-import '@storybook/addon-notes/register'
+import "@storybook/addon-actions/register";
+import "@storybook/addon-knobs/register";
+import "@storybook/addon-links/register";
+import "@storybook/addon-notes/register";
+import "@storybook/addon-storysource/register";
+import "@storybook/addon-viewport/register";
--- config/storybook/config.js
+++ config/storybook/config.js
@@ -1,18 +1,53 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import Vue from "vue";
 /* eslint-disable-next-line */
-import { configure, addDecorator } from "@storybook/vue";
-import apolloStorybookDecorator from "./ApolloStorybookDecorator.js";
-//import typeDefs from "../schema/typeDefinitions";
-import mocks from "./schema/mocks";
-import typeDefs from "./schema/typeDefs";
+import { configure } from "@storybook/vue";
+import { addParameters } from "@storybook/vue";
+
+const newViewports = {
+  xs: {
+    name: "xs 320",
+    styles: {
+      width: "320px",
+      height: "100%"
+    }
+  },
+  sm: {
+    name: "sm 640",
+    styles: {
+      width: "640px",
+      height: "100%"
+    }
+  },
+  md: {
+    name: "md 768",
+    styles: {
+      width: "768px",
+      height: "100%"
+    }
+  },
+  lg: {
+    name: "lg 1024",
+    styles: {
+      width: "1024px",
+      height: "100%"
+    }
+  },
+  xl: {
+    name: "xl 1280",
+    styles: {
+      width: "1280px",
+      height: "100%"
+    }
+  }
+};
+
+addParameters({ viewport: { viewports: newViewports } });
 
 import "../../src/assets/tailwind.css";
 import FontAwesomeIcon from "../../src/plugins/FontAwesomeIcon";
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
-
-addDecorator(apolloStorybookDecorator({ typeDefs, mocks, Vue }));
 
 const req = require.context("../../src/components", true, /.stories.js$/);
 

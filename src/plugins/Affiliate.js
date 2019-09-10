07d22cad6f47bce327b8083477cba0ebc02b0d00
Daniel da Rocha <danrocha@gmail.com>
2019-09-10 19:52:17 -0300

    add shopping links to book view

--- src/plugins/Affiliate.js
+++ src/plugins/Affiliate.js
@@ -13,6 +13,36 @@ const amazonAff = Affiliate({
       query: {
         tag: "awrdd-21" // will output a url with ?ref=my-tag
       }
+    },
+    {
+      hosts: ["amazon.co.uk", "www.amazon.co.uk"],
+      query: {
+        tag: "awrdd05-21"
+      }
+    },
+    {
+      hosts: ["amazon.fr", "www.amazon.fr"],
+      query: {
+        tag: "awrdd02-21"
+      }
+    },
+    {
+      hosts: ["amazon.es", "www.amazon.es"],
+      query: {
+        tag: "awrdd09-21"
+      }
+    },
+    {
+      hosts: ["amazon.it", "www.amazon.it"],
+      query: {
+        tag: "awrdd0b-21"
+      }
+    },
+    {
+      hosts: ["amazon.com.br", "www.amazon.com.br"],
+      query: {
+        tag: "awarded-20"
+      }
     }
   ]
 });

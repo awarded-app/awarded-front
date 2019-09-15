f26dd91749067f857a951fe830ac61d8faacc7cd
Daniel da Rocha <danrocha@gmail.com>
2019-09-15 08:01:42 -0300

    styles: put all color variables on App.vue

--- src/layouts/GenericLayout.vue
+++ src/layouts/GenericLayout.vue
@@ -32,72 +32,35 @@ export default {
 </script>
 
 <style>
-:root {
-  --homeMainColor: #e2e8f0;
-  --homeDarkGray: #718096;
-  --moviesMainColor: #ecc94b;
-  --moviesDarkGray: #2d3748;
-  --booksMainColor: #c53030;
-  --booksDarkGray: #e2e8f0;
-}
 body,
 html {
-  @apply bg-gray-800 text-white;
+  color: var(--homeTextColor);
+  background-color: var(--homeBgColor);
 }
 
 a.link-external::after {
-  @apply text-gray-500;
+  color: var(--homeFadedColor);
 }
 
 .text-faded {
-  @apply text-gray-500;
+  color: var(--homeFadedColor);
 }
 .text-main {
-  @apply text-white;
+  color: var(--homeTextColor);
 }
 
-a.movies-link,
-a.movies-link:visited {
-  background-image: linear-gradient(
-      transparent 2px,
-      var(--moviesMainColor) 2px,
-      var(--moviesMainColor) 4px,
-      transparent 4px
-    ),
-    linear-gradient(
-      transparent 2px,
-      var(--moviesDarkGray) 2px,
-      var(--moviesDarkGray) 4px,
-      transparent 4px
-    );
-}
 a.home-link,
 a.home-link:visited {
   background-image: linear-gradient(
       transparent 2px,
-      var(--homeMainColor) 2px,
-      var(--homeMainColor) 4px,
+      var(--homeAccentColor) 2px,
+      var(--homeAccentColor) 4px,
       transparent 4px
     ),
     linear-gradient(
       transparent 2px,
-      var(--homeDarkGray) 2px,
-      var(--homeDarkGray) 4px,
-      transparent 4px
-    );
-}
-a.books-link,
-a.books-link:visited {
-  background-image: linear-gradient(
-      transparent 2px,
-      var(--booksMainColor) 2px,
-      var(--booksMainColor) 4px,
-      transparent 4px
-    ),
-    linear-gradient(
-      transparent 2px,
-      var(--booksDarkGray) 2px,
-      var(--booksDarkGray) 4px,
+      var(--homeExtraFadedColor) 2px,
+      var(--homeExtraFadedColor) 4px,
       transparent 4px
     );
 }
@@ -105,19 +68,13 @@ a.books-link:visited {
 @supports (-ms-ime-align: auto) {
   a.link,
   a.home-link,
-  a.books-link,
-  a.movies-link,
   a.home-link:visited,
-  a.books-link:visited,
-  a.movies-link:visited,
   a.link:visited {
     background-size: 0% 2px, 100% 2px;
     padding-bottom: 2px;
   }
   a.link:hover,
-  a.home-link:hover,
-  a.books-link:hover,
-  a.movies-link:hover {
+  a.home-link:hover {
     background-size: 100% 2px;
   }
 }

dd21c3d5e53d8f3c8d4256944defa3075c5698f4
Daniel da Rocha <danrocha@gmail.com>
2019-09-27 10:41:15 -0300

    fix dateFormat filter

--- src/plugins/Filters.js
+++ src/plugins/Filters.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import { format, differenceInCalendarYears, startOfToday, getYear } from "date-fns";
 import { en } from "date-fns/locale";
 
-Vue.filter("formatDate", function(value, dateFormat = "DD.MMM.YY") {
+Vue.filter("formatDate", function(value, dateFormat = "dd.MMM.yy") {
   if (value) {
     return format(new Date(String(value)), dateFormat, { en });
   }

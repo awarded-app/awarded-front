421eb0e3e4c1ee39a3e672abbccf7ee39c71b05a
Daniel da Rocha <danrocha@gmail.com>
2019-09-29 20:56:12 -0300

    cleanup

--- src/plugins/FontAwesomeIcon.js
+++
@@ -1,8 +0,0 @@
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCaretRight, faCaretDown, faArrowCircleRight } from "@fortawesome/free-solid-svg-icons";
-
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-library.add(faCaretRight, faCaretDown, faArrowCircleRight);
-
-export default FontAwesomeIcon;
